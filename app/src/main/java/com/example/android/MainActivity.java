7556871ea0aef44f387888eb02b5409473b325d9
Tobby <73874976+gamjacode@users.noreply.github.com>
2020-11-30 13:05:50 +0900

    first comment

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //잘부탁드립니다.
     }
 }
